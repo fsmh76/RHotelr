1b0106709006d10f0724ccb6be78cfd87589e1c7
farha <fayhussaini@effatuniversity.edu.sa>
2023-07-12 15:46:03 +0300

    h

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public displayColumn: string[] = ['HotelID','IsReady','HotelsName','Location','LowestPrice','Currency','HotelRating'];
+  public dataSource: any = [];
+  
   title = 'RHotelr';
+  data = {};
+  
+  constructor(private http: HttpClient){
+    this.http.get('http://localhost:3000/Hotels').subscribe((data)=>{
+
+    this.data = data;
+    this.dataSource = data;
+  });
+
+  
+    applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+     
+  
+
+  }
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import {MatTableModule} from '@angular/material/table';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MatTableModule,
+    FormsModule,
+    MatFormFieldModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
